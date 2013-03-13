52c9bc9140476c46db6754bca0f8134a9cf26c29
Charles Offenbacher <charles.offenbacher@gmail.com>
2013-03-12 20:26:44 -0700

    committing users list

--- bjjprofile/views.py
+++ bjjprofile/views.py
@@ -1,9 +1,11 @@
 # Create your views here.
+from django.contrib.auth.models import User
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 
 def index(request):
-    return render_to_response('bjjprofile/index.html', {}, RequestContext(request))
+    users = User.objects.all()
+    return render_to_response('bjjprofile/list.html', {'users': users}, RequestContext(request))
 
 def show(request, name):
     return render_to_response('profile.html', {}, RequestContext(request))
