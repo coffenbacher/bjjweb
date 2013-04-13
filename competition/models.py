2c350083201dc0a9657c907dabc31df4bddc0c6d
Charles Offenbacher <charles.offenbacher@gmail.com>
2013-04-13 15:37:15 -0700

    pausing development

--- competition/models.py
+++ competition/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from technique.helpers import video_id
 from technique.models import Technique
 from django_extensions.db.models import TimeStampedModel
 
@@ -6,18 +7,28 @@ from django_extensions.db.models import TimeStampedModel
 class Competitor(TimeStampedModel):
     name = models.CharField(max_length=256)
     
+    def __unicode__(self):
+        return self.name
+        
 class MatchStyle(TimeStampedModel):
     name = models.CharField(max_length=256)
+    
+    def __unicode__(self):
+        return self.name
 
 class MatchType(TimeStampedModel):
     name = models.CharField(max_length=256)
+    
+    def __unicode__(self):
+        return self.name
 
 class Match(TimeStampedModel):
-    name = models.CharField(max_length=256)
     youtube_id = models.CharField(max_length=256)
     youtube_link = models.CharField(max_length=256)
     competitor_1 = models.ForeignKey(Competitor, related_name='c1s')
     competitor_2 = models.ForeignKey(Competitor, related_name='c2s')
+    event_date = models.DateField(null=True, blank=True)
+    event_name = models.CharField(max_length=256, null=True, blank=True)
     result = models.CharField(max_length=2,
         choices = (
             ('W1', 'Competitor 1 win'),
@@ -37,9 +48,13 @@ class Match(TimeStampedModel):
         if self.youtube_link:
             self.parse_youtube_id()
         return super(Match, self).save(*args, **kwargs)
+    
+    def __unicode__(self):
+        return '%s X %s' % (self.competitor_1, self.competitor_2)
 
 class TechniqueAppearance(TimeStampedModel):
     technique = models.ForeignKey(Technique)
     match = models.ForeignKey(Match)
+    competitor = models.ForeignKey(Competitor)
     second = models.PositiveIntegerField(verbose_name="At second")
     successful = models.BooleanField(default=True)
